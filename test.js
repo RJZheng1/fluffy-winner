85d85ffa2c147ef8452f5799f04d210db4f92a8e
Ivan Lin <ivlin112@gmail.com>
2016-03-28 11:46:06 -0400

    svg elements working

--- test.js
+++ test.js
@@ -1,22 +1,18 @@
-var canvas=document.getElementById("canvas");
-var ctx=canvas.getContext("2d");
+var svg = d3.select("body").append("svg")
+    .attr("width",1000)
+    .attr("height",500);
 
-var svg = document.getElementById("svg");
-ctx.beginPath();
 
-var projection = d3.geo.albersUsa()
-	.scale(1000)
-
-var path = d3.geo.path()
-	.projection(projection)
-	.context(ctx)
 
 d3.json("http://s3-us-west-2.amazonaws.com/vida-public/geo/us.json", function(data){
-	states = data.objects.states;
-	ctx.fillStyle="#FFAAAA"
-	for (i=0;i<states.geometries.length;i++){
-		path(topojson.feature(data,states.geometries[i]));
-		ctx.fill();
-	}
-	ctx.stroke();
+    svg.selectAll(".state")
+    //.feature() creates the path
+    //.features returns the individual elements of the collection
+	.data(topojson.feature(data,data.objects.states).features)
+	.enter().append("path")
+    //sets the map projection to albersUsa - how the map is displayed
+	.attr("d",d3.geo.path().projection(d3.geo.albersUsa()))
+	.attr("class",function(){return "state";})
+    //id that corresponds to ids from tsv file
+	.attr("id",function(state){return state.id;})
 });
